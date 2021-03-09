15aa31be6ace3fc5c335f5824565ff672c179593
ryand67 <rwdevenney@gmail.com>
2021-03-09 13:56:28 -0500

    intial

---
+++ components/PostForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PostForm() {
+    return (
+        <form action="">
+            <textarea name="" id="" cols={100} rows={15}></textarea>
+        </form>
+    )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import PostForm from '../components/PostForm';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Lrg</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Yellowtail&display=swap" rel="stylesheet"></link>
+      </Head>
+
+      <h1 className={styles.logo}>Lrg</h1>
+      <PostForm />
+    </div>
+  )
+}
